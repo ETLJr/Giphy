8af96019ce24430ec1039a76f7e3fbff88e98261
Elvis Lewis <Elvis5378@Gmail.com>
2023-04-05 17:32:54 -0400

    img problem

--- Giphy.js
+++ Giphy.js
@@ -12,9 +12,8 @@ $form.on('submit', async function getGif(e){
    const newGif = await axios.get('http://api.giphy.com/v1/gifs/search', {params:{q: $text, api_key}});
    const idx = Math.floor(Math.random() * newGif.data.data.length);
     const randomGif = newGif.data.data[idx]; 
-     $div.append(randomGif.images.original.url)
-     console.log(randomGif)
+    console.log(randomGif)
+     const img = $("<img>",{src: randomGif.images.orignal.url});
+     img.appendTo($div);
+     
 });
-
-
-
